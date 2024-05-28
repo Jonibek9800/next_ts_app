57ae44bfe72909a3873d5e30b6e88286bb117e0c
Jonibek M. Mahmudov <MahmudovJ@HUMO.local>
2024-05-28 14:22:55 +0500

    add env variable

--- src/shared/services/index.js
+++ src/shared/services/index.js
@@ -1,7 +1,4 @@
 import axios from "axios";
-
-export const BASE_URL = "http://localhost:3001";
-
 export const  instance = axios.create({
-    baseURL: BASE_URL
+    baseURL: process.env.API_URl
 })
