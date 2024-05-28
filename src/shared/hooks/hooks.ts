dc7b5a92f65043b0322d00f4f9d69d22b7bfdcd0
Jonibek M. Mahmudov <MahmudovJ@HUMO.local>
2024-05-28 13:57:43 +0500

    modify hooks page

--- src/shared/hooks/hooks.ts
+++ src/shared/hooks/hooks.ts
@@ -1,27 +1,5 @@
-import useSWR from "swr";
-import { getDishes } from "../services/dishes_service/dishes_service";
-import { IFood } from "../ui/interfaces";
 
-interface IDataResponse {
-  data: IFood[];
-  first: number;
-  items: number;
-  last: number;
-  next: number;
-  pages: number;
-}
 
-export function useDishes(currentPage: number) {
-  const { data, error, isLoading } = useSWR<IDataResponse>(
-    `/dishes?_page=${currentPage}`,
-    getDishes
-  );
 
-  return {
-    data: data,
-    error,
-    isLoading,
-  };
-}
 
 
