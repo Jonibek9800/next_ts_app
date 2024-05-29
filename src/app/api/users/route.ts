7fa5e673298e9d7a2aeb18f10c22d656dc44f763
Jonibek M. Mahmudov <MahmudovJ@HUMO.local>
2024-05-29 13:50:33 +0500

    fixed users rout

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -6,7 +6,7 @@ export async function GET(request: Request) {
   try {
     // const result =
     //   await sql`CREATE TABLE Pets ( Name varchar(255), Owner varchar(255) );`;
-    return NextResponse.json([...users], { status: 200 });
+    return NextResponse.json(users, { status: 200 });
   } catch (error) {
     return NextResponse.json({ error }, { status: 500 });
   }
